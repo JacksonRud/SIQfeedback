b4145ca5d472c8144f1fe420b7b613cf434fe985
mgenkin <genkin.matt@gmail.com>
2015-07-22 10:17:32 +0530

    changed buttons to radio buttons with smiley faces

--- routes/index.js
+++ routes/index.js
@@ -2,18 +2,16 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-var questionlist =  {
-	"a": 
-		{"question": "on a scale of whatever", "type": "scale",},
-	"b":
-		{"question": "tell me your life story", "type": "text"},
-	"c":
-		{"question": "how isnt it quite", "type": "text"}
-
-}
-
+var questionlist =  [
+	{"question": "on a scale of whatever", "type": "scale",},
+	{"question": "tell me your life story", "type": "text"},
+	{"question": "how isnt it quite", "type": "text"}
+]
 router.get('/', function(req, res, next) { res.render('index', {ql : questionlist });
 });
 
+router.get('/pretty', function(req, res, next){
+	res.render('indexPretty', { ql :questionlist});
+});
 
 module.exports = router;
